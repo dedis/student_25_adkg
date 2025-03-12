4118622481f9c04d4dea4efd20978a830931b2b4
HOT <theo@houle.pro>
2025-03-12 11:39:27 +0100

    Fix Bracha RBC implementation and add single test

--- rbc/bracha_test.go
+++ rbc/bracha_test.go
@@ -12,50 +12,38 @@ import (
 
 type TestNode struct {
 	iface networking.NetworkInterface[BrachaMsg]
-	rbc   *BrachaRBC
+	rbc   *BrachaRBC[kyber.Scalar]
 	stop  bool
 }
 
-func NewTestNode(iface networking.NetworkInterface[BrachaMsg], rbc *BrachaRBC) *TestNode {
+func NewTestNode(iface networking.NetworkInterface[BrachaMsg], rbc *BrachaRBC[kyber.Scalar]) *TestNode {
 	return &TestNode{
 		iface: iface,
 		rbc:   rbc,
 	}
 }
 
+type BrachaMsg networking.Message[MessageType, kyber.Scalar]
+
+func NewBrachaMsg(t MessageType, s kyber.Scalar) *BrachaMsg {
+	msg := networking.NewMessage[MessageType, kyber.Scalar](t, s)
+	return (*BrachaMsg)(msg)
+}
+
 func (n *TestNode) HandleMessage(t *testing.T, received BrachaMsg) {
 	t.Logf("[%d] Received message type: %d, content: %s", n.iface.GetID(), received.MsgType, received.MsgContent.String())
-	switch received.MsgType {
-	case Propose:
-		echo := n.rbc.ReceivePropose(received.MsgContent)
-		if echo {
-			t.Logf("[%d] Sending echo message", n.iface.GetID())
-			err := n.rbc.BroadcastEcho(received.MsgContent, n.iface)
-			if err != nil {
-				t.Logf("Error broadcasting echo msg: %s\n", err)
-			}
-		}
-	case Echo:
-		ready := n.rbc.ReceiveEcho(received.MsgContent)
-		if ready {
-			t.Logf("[%d] Sending Ready message", n.iface.GetID())
-			err := n.rbc.BroadcastReady(received.MsgContent, n.iface)
-			if err != nil {
-				t.Logf("Error broadcasting ready msg: %s\n", err)
-			}
-		}
-	case Ready:
-		val, finished, ready := n.rbc.ReceiveReady(received.MsgContent)
-		if finished {
-			t.Logf("Protocol finished, settled on value: %s\n", val)
-		} else if ready {
-			t.Logf("[%d] Sending ready msg", n.iface.GetID())
-			err := n.rbc.BroadcastReady(received.MsgContent, n.iface)
-			if err != nil {
-				t.Logf("Error broadcasting ready msg: %s\n", err)
-			}
-		}
+	msgType, val, send, output, finished := n.rbc.HandleMsg(received.MsgType, received.MsgContent)
+	if finished {
+		t.Logf("[%d] Algorithm finished, value: %s", n.iface.GetID(), output.String())
 	}
+
+	if send {
+		t.Logf("[%d] Sendig message of type %d", n.iface.GetID(), msgType)
+		msg := *NewBrachaMsg(msgType, val)
+		err := n.iface.Broadcast(msg)
+		require.NoError(t, err)
+	}
+	t.Logf("[%d] Nothing to do with the handled message", n.iface.GetID())
 }
 
 func (n *TestNode) Start(t *testing.T) {
@@ -78,6 +66,8 @@ func pred(kyber.Scalar) bool {
 	return true
 }
 
+// TestBrachaSimple creates a network of 3 nodes with a threshold of 1 and then lets one node start dealing and waits
+// sometime for the algorithm to finish and then check all nodes finished and settles on the same value that was dealt
 func TestBrachaSimple(t *testing.T) {
 	// Config
 	network := networking.NewFakeNetwork[BrachaMsg]()
@@ -96,8 +86,18 @@ func TestBrachaSimple(t *testing.T) {
 
 	n1 := nodes[0]
 	// Start RBC
-	err := n1.rbc.Broadcast(s, n1.iface)
+	msgType, val := n1.rbc.Deal(s)
+	msg := *NewBrachaMsg(msgType, val)
+	err := n1.iface.Broadcast(msg)
 	require.NoError(t, err)
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(4 * time.Second)
+
+	// Check that all nodes settled on the same correct value and all finished
+	for _, n := range nodes {
+		val := n.rbc.value
+		finished := n.rbc.finished
+		require.True(t, finished)
+		require.True(t, s.Equal(val))
+	}
 }

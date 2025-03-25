bb9029ac299b3be977d920a6681d94061d64805b
HOT <theo@houle.pro>
2025-03-25 14:27:56 +0100

    Redefine RBC interface and adapt BrachaRBC. Also add a message interface

--- rbc/rbc.go
+++ rbc/rbc.go
@@ -1,5 +1,7 @@
 package rbc
 
+import "student_25_adkg/messaging"
+
 type MessageType int8
 
 const (
@@ -8,10 +10,40 @@ const (
 	READY
 )
 
+// RBCMessage contains a message type and a content of the specified type
+type RBCMessage[M any] struct {
+	messaging.Message[M]
+	t       MessageType
+	content M
+}
+
+// NewRBCMessage construct a new message specific to RBC
+func NewRBCMessage[M any](t MessageType, content M) *RBCMessage[M] {
+	return &RBCMessage[M]{
+		t:       t,
+		content: content,
+	}
+}
+
+// Type returns the type of message
+func (m *RBCMessage[M]) Type() MessageType {
+	return m.t
+}
+
+// Content returns the content of the message
+func (m *RBCMessage[M]) Content() M {
+	return m.content
+}
+
+// RBC is an interface for a RBC protocol. M represents the type of the value that the protocol broadcasts
 type RBC[M any] interface {
-	// Deal creates the PROPOSE message to be broadcast
-	Deal(M) (MessageType, M)
-	// HandleMessage handles a message received. It returns a message to send if send is true, and an output value to
-	// for the algorithm if output is true and the value is stored in out.
-	HandleMessage(MessageType, M) (msgType MessageType, val M, send bool, out M, output bool)
+	// RBroadcast starts a reliable broadcast and returns the message to be broadcast
+	RBroadcast(M) *RBCMessage[M]
+	// HandleMessage handles a message received. It returns a message to broadcast. If it is nil, then nothing to do.
+	// And a boolean indicating if the protocol finished
+	HandleMessage(RBCMessage[M]) (*RBCMessage[M], bool)
+	// MarshalMessage converts an RBCMessage to bytes. Error is non-nil if something went wrong
+	MarshalMessage(RBCMessage[M]) ([]byte, error)
+	// UnmarshalMessage converts a byte array to an RBCMessage. Error is non-nil if something went wrong
+	UnmarshalMessage([]byte) (*RBCMessage[M], error)
 }

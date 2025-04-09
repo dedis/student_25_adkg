f905c0893d457f6c3c468e775d3aa6f63cd7efd0
HOT <theo@houle.pro>
2025-04-09 19:34:50 +0200

    update RBC interface

--- rbc/rbc.go
+++ rbc/rbc.go
@@ -1,49 +1,32 @@
 package rbc
 
-import "student_25_adkg/messaging"
+type InstanceID uint64
 
-type MessageType int8
-
-const (
-	PROPOSE MessageType = iota
-	ECHO
-	READY
-)
-
-// RBCMessage contains a message type and a content of the specified type
-type RBCMessage[M any] struct {
-	messaging.Message[M]
-	t       MessageType
-	content M
+type AuthenticatedMessageBroadcaster interface {
+	// Broadcast sends the given byte message to all nodes in the network.
+	Broadcast([]byte) error
 }
 
-// NewRBCMessage construct a new message specific to RBC
-func NewRBCMessage[M any](t MessageType, content M) *RBCMessage[M] {
-	return &RBCMessage[M]{
-		t:       t,
-		content: content,
-	}
+type AuthenticatedMessageReceiverHandler interface {
+	// AddHandler registers the given function to be called when new messages
+	// come in.
+	AddHandler(func([]byte) error)
 }
 
-// Type returns the type of message
-func (m *RBCMessage[M]) Type() MessageType {
-	return m.t
+// AuthenticatedMessageStream is an interface provided by the node to allow secure communication
+// with the network on a specific stream of messages. I.e. if this is interface is used
+// to start an RBC broadcast, all the message in the receiver should only contain messages related
+// to this RBC instance.
+type AuthenticatedMessageStream interface {
+	AuthenticatedMessageBroadcaster
+	AuthenticatedMessageReceiverHandler
 }
 
-// Content returns the content of the message
-func (m *RBCMessage[M]) Content() M {
-	return m.content
-}
-
-// RBC is an interface for a RBC protocol. M represents the type of the value that the protocol broadcasts
+// RBC is an interface for an RBC protocol. M represents the type of the value that the protocol broadcasts
 type RBC[M any] interface {
-	// RBroadcast starts a reliable broadcast and returns the message to be broadcast
-	RBroadcast(M) *RBCMessage[M]
-	// HandleMessage handles a message received. It returns a message to broadcast. If it is nil, then nothing to do.
-	// And a boolean indicating if the protocol finished
-	HandleMessage(RBCMessage[M]) (*RBCMessage[M], bool)
-	// MarshalMessage converts an RBCMessage to bytes. Error is non-nil if something went wrong
-	MarshalMessage(RBCMessage[M]) ([]byte, error)
-	// UnmarshalMessage converts a byte array to an RBCMessage. Error is non-nil if something went wrong
-	UnmarshalMessage([]byte) (*RBCMessage[M], error)
+	// RBroadcast blocks until the protocol is finished or an error occurred. The returned bool reflects this results
+	RBroadcast(M) error
+	// Listen expects to receive a PROPOSE message at some point that will start the protocol. This method
+	// blocks until the protocol is finished or an error is returned.
+	Listen() error
 }

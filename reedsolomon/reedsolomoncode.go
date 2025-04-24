82775606ab52434fdcecdd98ab22d79e452a60a6
HOT <theo@houle.pro>
2025-04-24 12:03:09 +0200

    remove generic type to 4 rounds RBC
    remove dependency to kyber.Scalar in 4Rounds RBC and its test
    
    put RS struct directly as argument
    
    Move Berlekamp-Welch implementation to its own file and modify test accordingly

--- reedsolomon/reedsolomoncode.go
+++ reedsolomon/reedsolomoncode.go
@@ -1,52 +1,24 @@
 package reedsolomon
 
-import (
-	"github.com/HACKERALERT/infectious"
-)
+// Encoding represents an encoded part of a message containing its byte value and its index in the encoded message
+type Encoding struct {
+	Idx int
+	Val []byte
+}
 
 type RSEncoder interface {
-	// Encode receives a message of k symbols and encodes it into a code of n symbols
-	Encode(msg []byte) ([]infectious.Share, error)
+	// Encode encodes the given message into a list of Encodings
+	Encode(msg []byte) ([]Encoding, error)
 }
 
 type RSDecoder interface {
-	// Decode takes a message of m symbols and tries to decode the original k symbols message
-	Decode(msg []infectious.Share) ([]byte, error)
+	// Decode takes a set of Encodings to be decoded. Returns the decoded bytes or an error
+	Decode(msg []Encoding) ([]byte, error)
 }
 
+// RSCodes is an interface representing an implementation of Reed-Solomon codes that
+// can encode and decode a message
 type RSCodes interface {
 	RSEncoder
 	RSDecoder
-}
-
-// BWCodes Berlekamp-Welch implementation of RS codes
-// from this library: https://pkg.go.dev/github.com/Picocrypt/infectious#NewFEC
-type BWCodes struct {
-	RSCodes
-	fec *infectious.FEC
-}
-
-func NewBWCodes(k, n int) *BWCodes {
-	fec, err := infectious.NewFEC(k, n)
-	if err != nil {
-		panic(err)
-	}
-	return &BWCodes{
-		fec: fec,
-	}
-}
-
-func (rs *BWCodes) Encode(msg []byte) ([]infectious.Share, error) {
-	shares := make([]infectious.Share, rs.fec.Total())
-	output := func(s infectious.Share) {
-		shares[s.Number] = s.DeepCopy()
-	}
-
-	err := rs.fec.Encode(msg, output)
-	return shares, err
-}
-
-func (rs *BWCodes) Decode(msg []infectious.Share) ([]byte, error) {
-	res, err := rs.fec.Decode(nil, msg)
-	return res, err
 }

3e81a3b25e3d0a6d43647e78c4d60cf1f1acf072
HOT <theo@houle.pro>
2025-05-18 17:48:01 +0200

    add node tests
    fix go.sum

--- rbc/node_test.go
+++ rbc/node_test.go
@@ -1,1 +1,37 @@
 package rbc
+
+import (
+	"context"
+	"student_25_adkg/networking"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNode_GetIdentifier(t *testing.T) {
+	node := NewNode(nil, int64(0))
+	require.Equal(t, int64(0), node.GetIdentifier())
+}
+
+func TestNode_Simple(t *testing.T) {
+
+	network := networking.NewFakeNetwork()
+
+	iface, err := network.JoinNetwork()
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	node := NewNode(iface, int64(0))
+
+	go func() {
+		require.NoError(t, node.Start(ctx))
+	}()
+
+	message := []byte("hello world")
+
+	err = node.BroadcastBytes(message)
+	require.NoError(t, err)
+
+	cancel()
+}

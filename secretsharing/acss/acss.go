9f1a48b28f3216857a6e0e01df1670b39b0e7062
HOT <theo@houle.pro>
2025-05-29 15:33:32 +0200

    define tests for ACSS (TDD!)

--- secretsharing/acss/acss.go
+++ secretsharing/acss/acss.go
@@ -1,1 +1,24 @@
 package acss
+
+import (
+	"context"
+	"student_25_adkg/networking"
+
+	"go.dedis.ch/kyber/v4"
+)
+
+type ACSS struct {
+	iface networking.NetworkInterface
+}
+
+func NewACSS(iface networking.NetworkInterface) *ACSS {
+	return &ACSS{iface: iface}
+}
+
+func (a *ACSS) Share(scalar kyber.Scalar) error {
+	return nil
+}
+
+func (a *ACSS) Start(ctx context.Context) error {
+	return nil
+}
